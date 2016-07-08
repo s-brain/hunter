3ba107e48ed26be22d599039031ec6057f5b51b9
Steve Brain <sbrain@imagus.com.au>
2016-07-08 17:29:28 +1000

    Added proto curl building windows

---
+++ cmake/projects/curl/hunter.cmake
@@ -0,0 +1,34 @@
+# Copyright (c) 2015, David Hirvonen
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+# Load used modules
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+# List of versions here...
+
+hunter_add_version(
+    PACKAGE_NAME
+    curl
+    VERSION
+    "7.49.1"
+    URL
+    "https://curl.haxx.se/download/curl-7.49.1.tar.bz2"
+    SHA1
+    50db69ee612d36a465e2fec3a35028e11c2766bb
+)
+
+# Pick a download scheme
+hunter_pick_scheme(DEFAULT url_sha1_autotools
+    WINDOWS
+    url_sha1_curl_windows
+)
+
+hunter_cacheable(curl)
+
+hunter_download(PACKAGE_NAME curl)
