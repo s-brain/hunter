a869668205a3c622955fa95f2e45a6b99bf398cb
Steve Brain <sbrain@imagus.com.au>
2016-07-14 14:38:56 +1000

    Curl build schemes now use cmake, and a hunterised version of curl

--- cmake/projects/CURL/hunter.cmake
+++ cmake/projects/CURL/hunter.cmake
@@ -15,29 +15,42 @@ include(hunter_pick_scheme)
 
 hunter_add_package(OpenSSL)
 hunter_add_package(ZLIB)
+hunter_add_package(LibSSH2)
 # List of versions here...
 
 hunter_add_version(
     PACKAGE_NAME
     CURL
     VERSION
-    "7.49.1"
+    "7.49.1-DEV"
     URL
-    "https://curl.haxx.se/download/curl-7.49.1.tar.bz2"
+    "https://github.com/hunter-packages/curl/archive/hunter-7.49.1-v0.zip"
     SHA1
-    50db69ee612d36a465e2fec3a35028e11c2766bb
+    fd72a153ca60fe85b57a2ac4eadedcfb38c3066d
 )
+
+
+if(BUILD_SHARED_LIBS)
+    SET(BUILD_CURL_STATIC OFF)
+else()
+    SET(BUILD_CURL_STATIC ON)
+endif()
+
+
+hunter_cmake_args(
+    CURL
+    CMAKE_ARGS
+        BUILD_CURL_TESTS=OFF
+        BUILD_CURL_EXE=OFF
+        CMAKE_USE_OPENSSL=ON
+        CMAKE_USE_LIBSSH2=ON
+        CURL_STATICLIB=${BUILD_CURL_STATIC}
+ )
+
+
 
 # Pick a download scheme
-hunter_pick_scheme(DEFAULT url_sha1_curl_autotools
-    WINDOWS
-    url_sha1_curl_windows
-)
-
-
-
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(CURL)
-
 hunter_download(PACKAGE_NAME CURL
-
-    PACKAGE_DEPENDS_ON OpenSSL ZLIB)
+                PACKAGE_DEPENDS_ON OpenSSL ZLIB LibSSH2)
